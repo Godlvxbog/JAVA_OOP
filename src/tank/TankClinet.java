d2fee20a28619b4b0393e29d969ea12fb01a0116
xbog <80210534@qq.com>
2016-11-26 21:55:52 +0800

    添加一个圆形图形，下一个，让坦克动起来

--- src/tank/TankClinet.java
+++ src/tank/TankClinet.java
@@ -9,10 +9,28 @@ import java.awt.event.WindowEvent;
  */
 public class TankClinet extends Frame{
 
+    //此方法默认会被调动，一段需要重新绘制的话
+    public void paint(Graphics g) {
+        //把图形颜色取出来，用完了，你在返回去
+        Color color =g.getColor();
+
+        g.setColor(Color.RED);
+        g.fillOval(30,50,30,30);//绘
+
+        g.setColor(color);//返回你原来的颜色
+        System.out.println("paintMethod");
+
+
+
+    }
+
     //弹出一个frame
-    public TankClinet(){
-        this.setLocation(120,20);
+    public void launchFrame(){
+        this.setLocation(400,300);
         this.setSize(800,600);
+
+        this.setBackground(Color.GREEN);
+
         this.setTitle("Tank_War");
         //不让改变大小
         this.setResizable(false);
@@ -30,6 +48,8 @@ public class TankClinet extends Frame{
 
     public static void main(String[] args) {
         TankClinet tankClinet = new TankClinet();
+        tankClinet.launchFrame();
+
 
 
     }
